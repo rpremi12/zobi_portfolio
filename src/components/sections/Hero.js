42003ccce7d34ca850e8ab8a2758ee16b5497ef6
Ryan Premi <rpremi@calpoly.edu>
2020-09-08 17:57:59 -0700

    template test 1

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,19 +61,19 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Ryan <span className="text-color-primary">Premi</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Developer | Consultant | Entrepreneur
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
+                  <Button tag="a" color="primary" wideMobile href="https://www.linkedin.com/in/ryan-premi/">
+                    LinkedIn
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
+                  <Button tag="a" color="dark" wideMobile href="https://github.com/rpremi12/">
+                    Github
                     </Button>
                 </ButtonGroup>
               </div>
