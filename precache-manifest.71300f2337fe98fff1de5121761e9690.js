0516a33c17dab08c307d961b511688d33a1e0d99
Ryan Premi <rpremi@calpoly.edu>
2022-01-27 13:35:16 -0800

    Updates

---
+++ precache-manifest.71300f2337fe98fff1de5121761e9690.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5025ace507684d758da9c2eee4800807",
+    "url": "/zobi_portfolio/index.html"
+  },
+  {
+    "revision": "a6678405e9b31a76f853",
+    "url": "/zobi_portfolio/static/css/main.24688f76.chunk.css"
+  },
+  {
+    "revision": "22c38c603e9ddb01263c",
+    "url": "/zobi_portfolio/static/js/2.8fa05dcc.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/zobi_portfolio/static/js/2.8fa05dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6678405e9b31a76f853",
+    "url": "/zobi_portfolio/static/js/main.03558f17.chunk.js"
+  },
+  {
+    "revision": "d1832a2873784e979271",
+    "url": "/zobi_portfolio/static/js/runtime-main.8e19dc63.js"
+  },
+  {
+    "revision": "9fd285f0217caed56368833912f81f2f",
+    "url": "/zobi_portfolio/static/media/PSU_logo2.9fd285f0.png"
+  },
+  {
+    "revision": "94ee2657bec453b25f5b4cf1e995de3d",
+    "url": "/zobi_portfolio/static/media/background1.94ee2657.jpg"
+  },
+  {
+    "revision": "b26d377025f1e85b7a270221449fac2c",
+    "url": "/zobi_portfolio/static/media/calpoly.b26d3770.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/zobi_portfolio/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/zobi_portfolio/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/zobi_portfolio/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/zobi_portfolio/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/zobi_portfolio/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/zobi_portfolio/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/zobi_portfolio/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/zobi_portfolio/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a4e512150f6bbfd917fad83b3e38d163",
+    "url": "/zobi_portfolio/static/media/logo-intel-2.a4e51215.jpg"
+  },
+  {
+    "revision": "d535a4b29620646348a2c3d1f181026b",
+    "url": "/zobi_portfolio/static/media/logo-transunion.d535a4b2.jpg"
+  }
+]);
